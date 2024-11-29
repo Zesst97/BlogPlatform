258967cc8eebbc29ae8e61b446ace7d15a92b57e
Zesst97 <152175833+Zesst97@users.noreply.github.com>
2024-11-29 15:10:38 +0300

    Update PostDetails.jsx

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,53 +1,49 @@
-/* eslint-disable react/jsx-no-bind */
-/* eslint-disable react/no-array-index-key */
+import { Popconfirm, message } from 'antd';
+import { format, parse } from 'date-fns';
+import React, { useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { useSelector } from 'react-redux';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
-import React, { useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import ReactMarkdown from 'react-markdown'
-import { parse, format } from 'date-fns'
-import { useSelector } from 'react-redux'
-import { Popconfirm, message } from 'antd'
+import { useDeletePostMutation, useGetPostsBySlugQuery } from '../../api/api';
+import defaultAvatar from '../../assets/images/avatar.png';
+import { ARTICLES } from '../../utils/routes/routesPath';
+import Error from '../Error';
+import Loading from '../Loading';
 
-import defaultAvatar from '../../assets/images/avatar.png'
-import Loading from '../Loading'
-import Error from '../Error'
-import { useDeletePostMutation, useGetPostsBySlugQuery } from '../../api/api'
-import { ARTICLES } from '../../utils/routes/routesPath'
-
-import classes from './PostDetails.module.scss'
+import classes from './PostDetails.module.scss';
 
 function PostDetails() {
-  const { slug } = useParams()
-  const { data, isLoading, isError, error } = useGetPostsBySlugQuery(slug)
-  const [deletePost, deleteResult] = useDeletePostMutation()
-  const userData = useSelector((state) => state.auth.user)
-  const localToken = localStorage.getItem('token')
-  const navigate = useNavigate()
+  const { slug } = useParams();
+  const { data, isLoading, isError, error } = useGetPostsBySlugQuery(slug);
+  const [deletePost, deleteResult] = useDeletePostMutation();
+  const userData = useSelector((state) => state.auth.user);
+  const localToken = localStorage.getItem('token');
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (deleteResult.isSuccess) {
-      navigate(ARTICLES)
+      navigate(ARTICLES);
     }
-  }, [deleteResult.isSuccess, navigate])
+  }, [deleteResult.isSuccess, navigate]);
 
-  const post = data?.article
+  const post = data?.article;
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   if (isError) {
-    return <Error error={error} />
+    return <Error error={error} />;
   }
 
   if (!post) {
-    return <p>Post Not Found</p>
+    return <p>Post Not Found</p>;
   }
 
   const formattedDate = post.createdAt
-    ? // eslint-disable-next-line quotes
-      format(parse(post.createdAt, "yyyy-MM-dd'T'HH:mm:ss.SSSX", new Date()), 'MMMM d, yyyy')
-    : 'Date unknown'
+    ? format(parse(post.createdAt, "yyyy-MM-dd'T'HH:mm:ss.SSSX", new Date()), 'MMMM d, yyyy')
+    : 'Date unknown';
 
   const tags = post.tagList
     .filter((tag) => tag.trim())
@@ -55,45 +51,38 @@ function PostDetails() {
       <div className={classes.info_tags} key={`${tag}-${index}`}>
         {tag}
       </div>
-    ))
+    ));
 
-  const markDownBody = post.body
+  const markDownBody = post.body;
 
-  const showController = post.author.username === userData?.username
+  const showController = post.author.username === userData?.username;
 
   const handleEditButton = () => {
-    navigate(`/articles/${slug}/edit`)
-  }
+    navigate(`/articles/${slug}/edit`);
+  };
 
   function confirm() {
-    message.success('Click on Yes')
-    const payload = { token: localToken, slug }
-    deletePost(payload)
+    message.success('Click on Yes');
+    const payload = { token: localToken, slug };
+    deletePost(payload);
   }
 
   function cancel() {
-    message.error('Click on No')
+    message.error('Click on No');
   }
 
   const userButtons = (
     <div className={classes.card_buttons}>
-      <Popconfirm
-        title="Are you sure to delete this post?"
-        onConfirm={confirm}
-        onCancel={cancel}
-        okText="Yes"
-        cancelText="No"
-      >
+      <Popconfirm title="Are you sure to delete this post?" onConfirm={confirm} onCancel={cancel} okText="Yes" cancelText="No">
         <button className={classes.user_delete_button} type="button">
           Delete
         </button>
       </Popconfirm>
-      <button className={classes.user_edit_button} onClick={handleEditButton} type="button">
+      <Link to="/articles/test-uoezkl/edit" className={classes.user_edit_button} onClick={handleEditButton}>
         Edit
-      </button>
+      </Link>
     </div>
-  )
-
+  );
   return (
     <div className={classes.wrapper}>
       <div className={classes.card}>
@@ -117,7 +106,7 @@ function PostDetails() {
               className={classes.card__avatar}
               src={post.author.image}
               onError={(e) => {
-                e.target.src = defaultAvatar
+                e.target.src = defaultAvatar;
               }}
               alt="Avatar"
             />
@@ -134,7 +123,7 @@ function PostDetails() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default PostDetails
+export default PostDetails;
